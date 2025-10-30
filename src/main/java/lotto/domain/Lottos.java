9fd20c6d6fb746ceb468df8610095156783f3b8e
h0108j <h0108j@naver.com>
2025-10-30 23:20:41 +0900

    feat(lottos): 로또 구매 개수 계산 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,9 +1,13 @@
 package lotto.domain;
 
+import static lotto.common.Constants.LOTTO_UNIT_PRICE;
+
 public class Lottos {
     private static int purchasePrice;
+    private static int PurchaseCount;
 
     public Lottos(int purchasePrice) {
         this.purchasePrice = purchasePrice;
+        this.PurchaseCount = purchasePrice / LOTTO_UNIT_PRICE;
     }
 }
